b3d505f3c77988de96c0af4b0e69a663444f0156
guvendolas <guvenndolas@gmail.com>
2022-09-19 13:56:58 +0300

    added chapter 5

---
+++ src/Chapters/Chapter_5/C05E02.java
@@ -0,0 +1,10 @@
+package Chapters.Chapter_5;
+
+import java.util.Scanner;
+
+public class C05E02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
