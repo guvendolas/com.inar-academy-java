ca769d3a261ff222af94c65d02b7295c8da6a5a8
guvendolas <guvenndolas@gmail.com>
2022-09-21 22:51:27 +0300

    öodifeded chapter 5 ex 7

--- src/Chapters/Chapter_5/C05E07.java
+++ src/Chapters/Chapter_5/C05E07.java
@@ -2,22 +2,21 @@ package Chapters.Chapter_5;
 
 public class C05E07 {
     public static void main(String[] args) {
-        double total = 0;
-        double tuition = 10_000;
-        int year = 0;
-        while (year < 14) {
-            tuition += 0.05 * tuition;
-            if (year == 10)
-                System.out.printf("Tuition in 10 years: $%1.2f\n", tuition);
+        int feePerYear = 10000;
+        int totalFee = 0;
 
-            if (year >= 10) {
-                total += tuition;
+        for (int i = 1; i <= 14; i++) {
+            feePerYear += feePerYear * .05;
+            if (i == 10) {
+                System.out.println("The tuition fee in ten year is $" + feePerYear);
             }
-            if (year == 13)
-                System.out.printf("In 10 years 4 years of tuition will cost $%1.2f", total);
-            year++;
+            if (i > 10) {
+                totalFee = totalFee + feePerYear;
+            }
         }
+        System.out.println("The total cost of 4 years tuition after the tenth year is $" + totalFee);
     }
-
-
 }
+
+
+
