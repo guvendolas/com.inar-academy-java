b9b55a73ac2df2f68f8522901b1b55f58f67b172
guvendolas <guvenndolas@gmail.com>
2022-09-24 21:09:45 +0300

    added chapter 5

--- src/Chapters/Chapter_5/C05E17.java
+++ src/Chapters/Chapter_5/C05E17.java
@@ -3,7 +3,7 @@ package Chapters.Chapter_5;
 import java.util.Scanner;
 
 public class C05E17 {
-    public static void main(String[] args) {
+    /**public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of line: ");
         int num = input.nextInt();
@@ -21,6 +21,42 @@ public class C05E17 {
         }
 
 
+    }
+
+
+
+}*/
+    public static void main(String[] args) {
+        System.out.print("Enter the number of lines: ");
+        Scanner input = new Scanner(System.in);
+        int sayi = input.nextInt();
+
+        for (int satir = 1; satir <= sayi; satir++) {
+            firstLoop(sayi, satir);
+            secondLoop(satir);
+            if(satir >= 2){
+                thirdLoop(satir);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void thirdLoop(int satir){
+        for (int sutun = 2; sutun <= satir ; sutun++) {
+            System.out.printf("%3d",sutun);
+        }
+    }
+
+    public static void secondLoop(int satir) {
+        for (int sutun = satir; sutun >= 1; sutun--) {
+            System.out.printf("%3d",sutun);
+        }
+    }
+
+    public static void firstLoop(int sayi, int satir) {
+        for (int sutun = sayi - satir; sutun > 0; sutun--) {
+            System.out.print("   ");
+        }
     }
 
 
