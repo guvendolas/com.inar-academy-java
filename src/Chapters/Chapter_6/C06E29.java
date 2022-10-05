81fcda3fc336bd0e1e71956595b67c57d78dbfcc
guvendolas <guvenndolas@gmail.com>
2022-10-05 23:10:44 +0300

    added chapter 6

--- src/Chapters/Chapter_6/C06E29.java
+++ src/Chapters/Chapter_6/C06E29.java
@@ -2,6 +2,10 @@ package Chapters.Chapter_6;
 
 public class C06E29 {
     public static void main(String[] args) {
-        
+        for (int i = 3; i < 1000; i++) {
+            if (C06E26.isPrime(i) && C06E26.isPrime(i + 2)) {
+                System.out.println("(" + i + "," + (i + 2) + ")");
+            }
+        }
     }
 }
