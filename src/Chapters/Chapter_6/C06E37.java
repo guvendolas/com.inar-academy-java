78e3107e48cac3597138c00a22ed90b5c350a3db
guvendolas <guvenndolas@gmail.com>
2022-10-06 14:10:04 +0300

    added chapter 6

--- src/Chapters/Chapter_6/C06E37.java
+++ src/Chapters/Chapter_6/C06E37.java
@@ -1,2 +1,26 @@
-package Chapters.Chapter_6;public class C06E37 {
+package Chapters.Chapter_6;
+
+import java.util.Scanner;
+
+public class C06E37 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("Enter the width expressed in integer format: ");
+        int width = input.nextInt();
+        System.out.println("The formatted number is: " + format(number, width));
+
+    }
+
+    public static String format(int number, int width) {
+        String s = (number + "");
+        if (s.length() < width) {
+            for (int i = width - s.length(); i > 0; i--) {
+                s = 0 + s;
+            }
+        }
+
+        return s;
+    }
 }
