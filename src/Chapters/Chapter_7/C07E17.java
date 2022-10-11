379ca3faba6ba468989102d09c954c064d5ef6e6
guvendolas <guvenndolas@gmail.com>
2022-10-11 15:11:04 +0300

    added chapter 7

--- src/Chapters/Chapter_7/C07E17.java
+++ src/Chapters/Chapter_7/C07E17.java
@@ -1,7 +1,50 @@
 package Chapters.Chapter_7;
 
+import java.util.Scanner;
+
 public class C07E17 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        String[] students = new String[input.nextInt()];
+        int[] scores = new int[students.length];
+
+        System.out.println("Enter the name a score for each student:");
+        for (int i = 0; i < students.length; i++) {
+            System.out.print("Student " + (i + 1) + ": ");
+            students[i] = input.next();
+            System.out.print("Score: ");
+            scores[i] = input.nextInt();
+        }
+
+        sortDecreasing(students, scores);
+
+        for (String i : students) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sortDecreasing(String[] strs, int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int max = nums[i];
+            int maxIndex = i;
+            String temp;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > max) {
+                    max = nums[j];
+                    maxIndex = j;
+                }
+            }
+            if (maxIndex != i) {
+                temp = strs[i];
+                strs[i] = strs[maxIndex];
+                strs[maxIndex] = temp;
+
+                nums[maxIndex] = nums[i];
+                nums[i] = max;
+            }
+        }
     }
 }
