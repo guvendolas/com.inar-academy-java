b209cd99b37417744e8e76d6a1b9d42fe6dc2a2b
guvendolas <guvenndolas@gmail.com>
2022-10-12 12:20:18 +0300

    added chapter 7

---
+++ src/Chapters/Chapter_7/C07E20.java
@@ -0,0 +1,39 @@
+package Chapters.Chapter_7;
+
+import java.util.Scanner;
+
+public class C07E20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < numbers.length ; i++)
+            numbers[i] = input.nextDouble();
+
+        selectionSort(numbers);
+
+        for (double e: numbers)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void selectionSort(double[] list) {
+        for (int i = list.length - 1; i >= 0; i--) {
+            double currentMax = list[i];
+            int currentMaxIndex = i;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (currentMax < list[j]) {
+                    currentMax = list[j];
+                    currentMaxIndex = j;
+                }
+            }
+
+            if (currentMaxIndex != i) {
+                list[currentMaxIndex] = list[i];
+                list[i] = currentMax;
+            }
+        }
+    }
+}
