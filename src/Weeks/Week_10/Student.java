8d3c39cefc7318431cef9ceca6082f37b32f7463
guvendolas <guvenndolas@gmail.com>
2022-10-15 20:36:59 +0300

    added week 10

---
+++ src/Weeks/Week_10/Student.java
@@ -0,0 +1,12 @@
+package Weeks.Week_10;
+
+public class Student {
+    String name;
+    String lastName;
+    int age;
+    String email;
+
+    public void changeEmail(String newEmail){
+        this.email = newEmail;
+    }
+}
