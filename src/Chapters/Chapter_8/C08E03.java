21814307afe6a791e40479d6fea9b81b8a75de9f
guvendolas <guvenndolas@gmail.com>
2022-10-17 14:55:11 +0300

    added chapter 8

--- src/Chapters/Chapter_8/C08E03.java
+++ src/Chapters/Chapter_8/C08E03.java
@@ -14,15 +14,46 @@ public class C08E03 {
 
         char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
 
+        int[][] correctCounts = new int[answers.length][2];
+
+        // Grade all answers
         for (int i = 0; i < answers.length; i++) {
-            int correctCount = 0;
+            // Grade one student
             for (int j = 0; j < answers[i].length; j++) {
-                if (answers[i][j] == keys[j])
-                    correctCount++;
+                if (answers[i][j] == keys[j]) {
+                    correctCounts[i][0] = i;
+                    correctCounts[i][1]++;
+                }
             }
 
-            System.out.println("Student " + i + "'s correct count is " + correctCount);
+        }
+        sort(correctCounts);
 
+        for (int row = 0; row < correctCounts.length; row++) {
+            System.out.println("Student " + correctCounts[row][0] +
+                    "'s correct count is " + correctCounts[row][1]);
+        }
+    }
+
+    public static void sort(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            int minCol1 = m[i][1];
+            int minCol0 = m[i][0];
+            int minIndex = i;
+
+            for (int j = i + 1; j < m.length; j++) {
+                if (minCol1 > m[j][1]) {
+                    minCol1 = m[j][1];
+                    minCol0 = m[j][0];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                m[minIndex][1] = m[i][1];
+                m[minIndex][0] = m[i][0];
+                m[i][1] = minCol1;
+                m[i][0] = minCol0;
+            }
         }
     }
 }
