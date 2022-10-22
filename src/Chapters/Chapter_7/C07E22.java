9a8b4d71588a9d814198a72e5c3e448259c055c2
guvendolas <guvenndolas@gmail.com>
2022-10-22 11:12:02 +0300

    modifeded chapter 7 ex. 22

--- src/Chapters/Chapter_7/C07E22.java
+++ src/Chapters/Chapter_7/C07E22.java
@@ -1,99 +1,109 @@
 package Chapters.Chapter_7;
 
+import java.util.Arrays;
+
 public class C07E22 {
     public static void main(String[] args) {
-        char[] board;
+        int[] chessTable = new int[8];
+        int row = 0;
+        int count = 0;
 
-
-        do {
-            board = getNewBoard();
-
-            placeQueens(board);
-
-        } while (isAttacking(board));
-
-        print(board);
-    }
-
-    public static void placeQueens(char[] board) {
-        int location;
-        for (int i = 0; i < 8; i++) {
-            do {
-                location = placeQueens();
-            } while (isOccupied(board[location]));
-            board[location] = 'Q';
-        }
-    }
-
-    public static int placeQueens() {
-        return (int) (Math.random() * 64);
-    }
-
-    public static boolean isAttacking(char[] board) {
-        return isSameRow(board) || isSameColumn(board) || isSameDiagonal(board);
-    }
-
-    public static boolean isSameRow(char[] board) {
-        int[] rows = new int[8];
-        for (int i = 0; i < board.length; i++) {
-            if (isOccupied(board[i])) {
-                rows[getRow(i)]++;
+        while (row < chessTable.length) {
+            count++;
+            if (isThereAPLaceToPutAQueen(chessTable, row)) {
+                putAQueen(chessTable, row);
+                row++;
+            } else {
+                row = 0;
             }
-            if (rows[getRow(i)] > 1)
+        }
+        displayChessTable(chessTable, count);
+
+    }
+
+    // Checks for the next row int chess table whether there is place for queen
+    public static boolean isThereAPLaceToPutAQueen(int[] chessTable, int row) {
+        boolean[] checkChessTable = new boolean[8];
+        fill(checkChessTable, true);// You may also use java.util.Arrays.fill(checkChessTable, true);
+        // Turns into false the places that can't be queen put
+        for (int i = 0; i < row; i++) {
+            checkChessTable[chessTable[i]] = false;
+            if (( chessTable[i] + row - i) < 8) {
+                checkChessTable[row - i + chessTable[i]] = false;
+            }
+            if ((chessTable[i] - (row - i)) > 0) {
+                checkChessTable[chessTable[i] - (row - i)] = false;
+            }
+        }
+
+        for (int i = 0; i < checkChessTable.length; i++) {
+            if (checkChessTable[i]) {
                 return true;
-        }
-        return false;
-    }
-
-    public static boolean isSameColumn(char[] board) {
-        int[] columns = new int[8];
-        for (int i = 0; i < board.length; i++) {
-            if (isOccupied(board[i])) {
-                columns[getColumn(i)]++;
-            }
-            if (columns[getColumn(i)] > 1)
-                return true;
-        }
-        return false;
-    }
-
-    public static boolean isSameDiagonal(char[] board) {
-        for (int i = 0; i < board.length; i++) {
-            if (isOccupied(board[i])) {
-                for (int j = 0; j < board.length; j++) {
-                    if (isOccupied(board[j]) && Math.abs(getColumn(j) - getColumn(i)) ==
-                            Math.abs(getRow(j) - getRow(i)) && j != i) {
-                        return true;
-                    }
-                }
             }
         }
         return false;
     }
 
-    public static boolean isOccupied(char x) {
-        return x == 'Q';
-    }
-
-    public static char[] getNewBoard() {
-        char[] board = new char[64];
-        for (int i = 0; i < board.length; i++)
-            board[i] = ' ';
-        return board;
-    }
-
-    public static void print(char[] board) {
-        for (int i = 0; i < board.length; i++) {
-            System.out.print(
-                    "|" + ((getRow(i + 1) == 0) ? board[i] + "|\n" : board[i]));
+    public static void fill(boolean[] checkChessTable, boolean b) {
+        for (int i = 0; i < checkChessTable.length; i++) {
+            checkChessTable[i] = b;
         }
     }
 
-    public static int getRow(int index) {
-        return index % 8;
+    // Puts a queen to row randomly
+    public static void putAQueen(int[] chessTable, int row) {
+        int queensPlace;
+        while (true) {
+            queensPlace = (int) (Math.random() * 8);
+            if (isThatPlaceAvailable(chessTable, queensPlace, row)) {
+                break;
+            }
+        }
+        chessTable[row] = queensPlace;
     }
 
-    public static int getColumn(int index) {
-        return index / 8;
+    // Checks the queens place correctness
+    private static boolean isThatPlaceAvailable(int[] chessTable, int queensPlace, int row) {
+        boolean[] checkSuitable = new boolean[8];
+        Arrays.fill(checkSuitable, true);
+        // Turn into false the places that queen can't be putted
+        for (int i = 0; i < row; i++) {
+            checkSuitable[chessTable[i]] = false;
+            if ((row - i + chessTable[i]) < 8) {
+                checkSuitable[row - i + chessTable[i]] = false;
+            }
+            if (chessTable[i] - (row - i) > 0) {
+                checkSuitable[chessTable[i] - (row - i)] = false;
+            }
+        }
+        return checkSuitable[queensPlace];
+
+    }
+
+    public static void displayChessTable(int[] chessTable, int count) {
+        System.out.println("It takes " + count + " times");
+
+        for (int i = 0; i < chessTable.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < chessTable[i]; j++) {
+                System.out.print(" |");
+            }
+            System.out.print("Q| ");
+
+            for (int j = chessTable[i] + 1; j < chessTable.length; j++) {
+                System.out.print("| ");
+            }
+            System.out.println();
+        }
     }
 }
+/*  0 _ _ Q _ _ _ _ _
+    1 _ _ _ _ _ _ _ _
+    2 Q _ _ _ _ _ _ _ chessTable[i] - (row - i)
+    3 _ _ _ _ _ Q _ _ (row - i) + ChessTAble[i]
+    4 _ _ _ _ _ _ _ _
+    5 _ _ Q _ _ _ _ _ chessTAble[i]
+    6 _ _ _ _ _ _ _ _
+    7 _ _ _ _ _ _ _ _
+ */
+
