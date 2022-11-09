3f149491a6f008111526749395e774f88b59156f
guvendolas <guvenndolas@gmail.com>
2022-11-09 20:28:16 +0300

    added chapter 10

---
+++ src/Chapters/Chapter_10/E_09/C10E09.java
@@ -0,0 +1,20 @@
+package Chapters.Chapter_10.E_09;
+
+public class C10E09 {
+    public static void main(String[] args) {
+        Course c1 = new Course("DenemeKursu");
+        c1.addStudent("Serhat");
+        c1.addStudent("Ertugrul");
+        c1.addStudent("Sinan");
+        c1.displayStudents();
+
+        System.out.println("-------------------------");
+        c1.dropStudent("Serhat");
+        c1.displayStudents();
+
+        System.out.println("-------------------------");
+        c1.clear();
+        c1.displayStudents();
+
+    }
+}
--- src/Chapters/Chapter_10/E_09/Course.java
+++ src/Chapters/Chapter_10/E_09/Course.java
@@ -1,17 +1,19 @@
 package Chapters.Chapter_10.E_09;
 
 public class Course {
-    private String courseName;
-    private String[] students = new String[100];
+    private String name;
+    private String[] students;
     private int numberOfStudents;
 
-    public Course(String courseName) {
-        this.courseName = courseName;
+    public Course(String name) {
+        this.name = name;
+        students = new String[10];
     }
-    public void addStudent(String student) {
-        students[numberOfStudents] = student;
-        numberOfStudents++;
+
+    public String getName() {
+        return name;
     }
+
     public String[] getStudents() {
         return students;
     }
@@ -20,10 +22,50 @@ public class Course {
         return numberOfStudents;
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
-    public void dropStudent(String student) {
+    public void addStudent(String student) {
+        if (numberOfStudents >= student.length()) {
+            String[] tempStudentList = new String[students.length * 2];
+            System.arraycopy(students, 0, tempStudentList, 0, students.length);
+            students = tempStudentList;
+        }
 
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+
+    }
+
+    // A B D F G H null
+    public void dropStudent(String student) {
+        int indexOfStudent = findTheSpecifiedStudent(student);
+        if (indexOfStudent == -1) {
+            System.out.println("There is not any student like " + student);
+            return;
+        }
+
+        for (int startPoint = indexOfStudent; startPoint < numberOfStudents - 1; startPoint++) {
+            students[startPoint] = students[startPoint + 1];
+        }
+        numberOfStudents--;
+
+    }
+
+    public int findTheSpecifiedStudent(String student) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(student)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        students = new String[10];
+        numberOfStudents = 0;
+    }
+
+    public void displayStudents() {
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println(students[i]);
+        }
     }
 }
