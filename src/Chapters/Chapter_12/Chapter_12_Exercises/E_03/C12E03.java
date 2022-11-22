ba8b95574660353271693161462ce071bd9fd28f
guvendolas <guvenndolas@gmail.com>
2022-11-22 19:34:23 +0300

    added chapter 12

---
+++ src/Chapters/Chapter_12/Chapter_12_Exercises/E_03/C12E03.java
@@ -0,0 +1,28 @@
+package Chapters.Chapter_12.Chapter_12_Exercises.E_03;
+
+import java.util.Scanner;
+
+public class C12E03 {
+    public static void main(String[] args) {
+
+        int[] randomArray = new int[100];
+
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (1 + Math.random() * 10_000);
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an index in the array for which to display it's value: ");
+
+        try {
+            int indexValue = input.nextInt();
+            System.out.println(randomArray[indexValue]);
+
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Out Of Bounds");
+        }
+
+    }
+
+}
